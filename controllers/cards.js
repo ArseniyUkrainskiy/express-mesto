617ff151c2eb1b8d62f57fe6e57932f2eb06a623
Arseniy <ArseniyUkrainskiy@yandex.ru>
2021-07-06 22:05:11 +0300

    cards{}

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,13 +3,13 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ cards }))
+    .then((cards) => res.send(cards))
     .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка.' }));
 };
 module.exports.addCard = (req, res) => {
   const { name, link, owner = req.user._id } = req.body;
   Card.create({ name, link, owner })
-    .then((card) => res.send({ card }))
+    .then((card) => res.send(card))
     .catch((err) => (err.name === 'ValidationError'
       ? res.status(400).send({
         message: 'Переданы некорректные данные при создании карточки.',
@@ -20,7 +20,7 @@ module.exports.addCard = (req, res) => {
 };
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send({ card }))
+    .then((card) => res.send(card))
     .catch((err) => (err.name === 'CastError'
       ? res.status(404).send({
         message: 'Карточка с указанным _id не найдена.',
@@ -35,7 +35,7 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.send({ card }))
+    .then((card) => res.send(card))
     .catch((err) => (err.name === 'ValidationError'
       ? res.status(400).send({
         message: 'Переданы некорректные данные для постановки лайка. ',
@@ -50,7 +50,7 @@ module.exports.deleteLikeOnCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.send({ card }))
+    .then((card) => res.send(card))
     .catch((err) => (err.name === 'ValidationError'
       ? res.status(400).send({
         message: 'Переданы некорректные данные для снятия лайка. ',
