c649b076decbf9918953ecf5f0d1974098e596d9
Arseniy <ArseniyUkrainskiy@yandex.ru>
2021-07-19 22:34:50 +0300

    Revert "httpOnly Cookie"
    This reverts commit 0c3a5985a0502d7616f660037794a5bfb27ed482.

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,4 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-
-module.exports.login = (req, res) => {
-  const { email, password } = req.body;
-  return User.findUserByCredentials(email, password)
-    .then((user) => {
-      // аутентификация успешна! пользователь в переменной user
-      const token = jwt.sign({ _id: user._id }, { expiresIn: '7d' });
-
-      // вернём токен
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
-      /* res.send({ token }); */
-    })
-    .catch((err) => {
-      // ошибка аутентификации
-      res
-        .status(401)
-        .send({ message: err.message });
-    });
-};
 
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -30,21 +9,16 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.addUser = (req, res) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body;
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name, about, avatar, email, password: hash,
-    })
-      .then((user) => res.status(200).send(user))
-      .catch((err) => (err.name === 'ValidationError'
-        ? res.status(400).send({
-          message: 'Переданы некорректные данные при создании пользователя.',
-        })
-        : res.status(500).send({
-          message: 'На сервере произошла ошибка.',
-        }))));
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => (err.name === 'ValidationError'
+      ? res.status(400).send({
+        message: 'Переданы некорректные данные при создании пользователя.',
+      })
+      : res.status(500).send({
+        message: 'На сервере произошла ошибка.',
+      })));
 };
 
 module.exports.getUser = (req, res) => {
